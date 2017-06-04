77e604840924e2b0c4a495db9720ec4c991577a8
Tek Hudson <tek.hudson@gmail.com>
2017-06-04 09:42:33 -0700

    Add some common views and routes for races, classes, spells

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "races#index"
+
+  resources :classes, only: [:index]
+  resources :races, only: [:index]
+
+  resources :spells, only: [:index]
 end
