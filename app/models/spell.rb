5ffb8783362e9d6cf6be27c4ae24a57e79844499
Tek Hudson <tek.hudson@gmail.com>
2017-06-13 11:39:53 -0700

    Add case insensativity to spell name and description search

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -2,11 +2,11 @@ class Spell < ApplicationRecord
   def self.all_for_display (params)
     results = []
     if params[:name].present? && params[:description].present?
-      results = Spell.where("name LIKE ? AND description LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%").order(:name)
+      results = Spell.where("lower(name) LIKE ? AND description LIKE ?", "%#{params[:name]}%".downcase, "%#{params[:name]}%".downcase).order(:name)
     elsif params[:name].present?
-      results = Spell.where("name LIKE ? ", "%#{params[:name]}%").order(:name)
+      results = Spell.where("lower(name) LIKE ? ", "%#{params[:name]}%".downcase).order(:name)
     elsif params[:description].present?
-      results = Spell.where("description LIKE ? ", "%#{params[:description]}%").order(:name)
+      results = Spell.where("lower(description) LIKE ? ", "%#{params[:description]}%".downcase).order(:name)
     elsif params[:klass].present?
       results = Spell.where("? = ANY (klass)", "#{params[:klass]}").order(:name)
     else
