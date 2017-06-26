d5e7ab717b29098640f1b5ea28cd39108d86d354
Tek Hudson <tek.hudson@gmail.com>
2017-06-26 10:27:57 -0700

    Use modal to add combatant

--- app/assets/javascripts/data-tables-init.coffee
+++ app/assets/javascripts/data-tables-init.coffee
@@ -10,7 +10,14 @@ $(document).on "turbolinks:load", ->
     unless table.hasClass("sort-asc")
       options["order"] = [[ 0, "desc" ]]
 
+    if table.hasClass("sort-1-desc")
+      options["order"] = [[ 1, "desc" ]]
+
+    if table.hasClass("sort-2-desc")
+      options["order"] = [[ 2, "desc" ]]
+
     if table.hasClass("no-paging")
       options["paging"] = false
+
 
     table.DataTable(options)
--- app/controllers/combatants_controller.rb
+++ app/controllers/combatants_controller.rb
@@ -7,6 +7,16 @@ class CombatantsController < ApplicationController
     redirect_to edit_combat_tracker_path(combatant_params[:combat_tracker_id])
   end
 
+  def update
+    combatant = Combatant.find(params[:id])
+
+    if combatant.update(combatant_params)
+      redirect_to edit_combat_tracker_path(combatant.combat_tracker_id)
+    else
+      redirect_to edit_combat_tracker_path(ct), alert: ct.errors.full_messages.to_sentence
+    end
+  end
+
   def destroy
     combatant = Combatant.find(params[:id])
 
@@ -19,7 +29,7 @@ class CombatantsController < ApplicationController
 
   private
 
-  def combatant_params(params)
+  def combatant_params
     params.require(:combatant).permit(:combat_tracker_id, :name, :init, :max_hp, :hp, :ac, :dc)
   end
 end
