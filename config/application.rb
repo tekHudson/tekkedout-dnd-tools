51d74aa5ebb17f7a56e21b028853c23ce68e3dfe
Tek Hudson <tek.hudson@gmail.com>
2017-07-03 11:17:42 -0700

    Remove twitter config for now

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require "twitter"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,12 +11,5 @@ module DndTools
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    Twitter.configure do |config|
-      config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
-      config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
-      config.oauth_token = ENV["TWITTER_OAUTH_TOKEN"]
-      config.oauth_token_secret = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
-    end
   end
 end
