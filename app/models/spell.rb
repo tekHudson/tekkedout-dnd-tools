ded00e4e2fa39d71f90f04a374fd2313ab8ad2ce
Tek Hudson <tek.hudson@gmail.com>
2017-07-07 00:27:39 -0700

    Rubocop cleanup

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,18 +1,13 @@
 class Spell < ApplicationRecord
   # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
   def self.all_for_display(klass = nil, name = nil)
     klass.capitalize! if klass
     name.downcase! if name
+    klass_and_name = klass.present? && name.present?
 
-    if klass.present? && name.present?
-      return where("? = ANY (klass)", klass.to_s).where("lower(name) ILIKE ?", "%#{name}%")
-    elsif klass.present?
-      return where("? = ANY (klass)", klass.to_s)
-    elsif name.present?
-      return where("lower(name) ILIKE ?", "%#{name}%")
-    else
-      return all
-    end
+    return where("? = ANY (klass)", klass.to_s).where("lower(name) ILIKE ?", "%#{name}%") if klass_and_name
+    return where("? = ANY (klass)", klass.to_s) if klass.present?
+    return where("lower(name) ILIKE ?", "%#{name}%") if name.present?
+    all
   end
 end
